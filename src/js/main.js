cc4a29e57da1502415027d8cf3413efd62c82cda
Pau10 <paulo.yorik@gmail.com>
2016-03-28 12:33:58 +0300

    first markup upload

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+/*
+ * Third party
+ */
+//= vendor/jquery-1.11.2.min.js
+//= vendor/modernizr-2.8.3.min.js
+
+/*
+ * Plugins
+ */
+//= plugins/slick.js
+
+/*
+ * Custom
+ */
+//= partials/app.js
---
+++ src/js/partials/app.js
@@ -0,0 +1,99 @@
+// ********** Inits
+
+// Slick horizontal gallery
+$('.content-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false,
+  swipe: false,
+  speed: 500,
+});
+
+// Slick vertical section slider
+$('.container-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false,
+  vertical: true,
+  arrows: false,
+  swipe: true,
+  verticalSwiping: true,
+  infinite: false,
+  speed: 500,
+});
+
+
+// ********** Customs
+
+// Auto height for .full-height blocks
+function fullHeight(){
+  var contHeight = $('.container').height();
+  $('.full-height').css('height', contHeight);
+};
+function vertSliderNavDotsCenter(){
+  // Vertical center slick-slider dots
+  var bodyHeight = $('body').height();
+  var slickDotsHeight = $('.vertical-slider-nav-dots').height();
+  $('.vertical-slider-nav-dots').css('top', bodyHeight/2-slickDotsHeight/2);
+};
+
+
+
+
+
+
+
+
+// Vertical-slider-nav-dots
+
+var dotsNav = $('.vertical-slider-nav-dots');
+
+$('.container-slider .container-slide').each( function ( IDX ) {
+
+  var dotsNavItem = $('<div/>', {class: 'dot', 'data-index' : IDX });
+
+  dotsNav.append ( dotsNavItem );
+
+  dotsNavItem.on('click', function(){
+    var dotIndex = $(this).data('index');
+    $('.container-slider').slick('slickGoTo', dotIndex );
+  });
+});
+
+$($('.dot').get(0)).addClass('active');
+
+$('.container-slider').on('afterChange', function(event, slick, currentSlide){
+  $('.vertical-slider-nav-dots .dot').removeClass('active');
+  $($('.dot').get(currentSlide)).addClass('active');
+});
+$('.content-slider').on('afterChange', function(event, slick, currentSlide){
+  return false;
+});
+
+$( document ).ready(function() {
+  $('.intro-video').get(0).play();
+  $('.menu-item-video').get(0).play();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fullHeight();
+vertSliderNavDotsCenter();
+$( window ).resize(function() {
+  fullHeight();
+  vertSliderNavDotsCenter();
+});
