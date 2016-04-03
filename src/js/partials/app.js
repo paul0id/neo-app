8f0c6bb0a69c2d6da4809f93d68451c74c6aee0e
Pau10 <paulo.yorik@gmail.com>
2016-04-03 19:31:46 +0300

    complete contact pages

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -38,30 +38,17 @@ function vertSliderNavDotsCenter(){
 };
 
 
-
-
-
-
-
-
 // Vertical-slider-nav-dots
-
 var dotsNav = $('.vertical-slider-nav-dots');
-
 $('.container-slider .container-slide').each( function ( IDX ) {
-
   var dotsNavItem = $('<div/>', {class: 'dot', 'data-index' : IDX });
-
   dotsNav.append ( dotsNavItem );
-
   dotsNavItem.on('click', function(){
     var dotIndex = $(this).data('index');
     $('.container-slider').slick('slickGoTo', dotIndex );
   });
 });
-
 $($('.dot').get(0)).addClass('active');
-
 $('.container-slider').on('afterChange', function(event, slick, currentSlide){
   $('.vertical-slider-nav-dots .dot').removeClass('active');
   $($('.dot').get(currentSlide)).addClass('active');
@@ -70,16 +57,14 @@ $('.content-slider').on('afterChange', function(event, slick, currentSlide){
   return false;
 });
 
+
+// Autoplay for all videos
 $( document ).ready(function() {
   $('video').get(0).play();
 });
 
 
-
-
-
-
-
+// Auto height usage
 fullHeight();
 vertSliderNavDotsCenter();
 $( window ).resize(function() {
